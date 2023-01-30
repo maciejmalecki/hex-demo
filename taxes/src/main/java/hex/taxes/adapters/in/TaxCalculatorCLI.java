66397735791a378a5d113cd83c3d4db19bde1065
Maciej Malecki <malecki.maciej@gmail.com>
2023-01-30 22:23:09 +0100

    Inbound adapter (CLI)

---
+++ taxes/src/main/java/hex/taxes/adapters/in/TaxCalculatorCLI.java
@@ -0,0 +1,32 @@
+package hex.taxes.adapters.in;
+
+import hex.taxes.adapters.out.InMemoryTaxRepository;
+import hex.taxes.application.TaxCalculator;
+import hex.taxes.application.ports.in.ForCalculatingTaxes;
+
+import java.math.BigDecimal;
+
+public class TaxCalculatorCLI {
+
+    private final ForCalculatingTaxes forCalculatingTaxes;
+
+    TaxCalculatorCLI(ForCalculatingTaxes forCalculatingTaxes) {
+        this.forCalculatingTaxes = forCalculatingTaxes;
+    }
+
+    void calculate(BigDecimal amount) {
+        var amountAfterTax = forCalculatingTaxes.taxOn(amount);
+        System.out.printf("Amount after tax for %.2f is %.2f.", amount, amountAfterTax);
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar taxes.jar <amount>");
+            System.exit(1);
+        }
+
+        var amount = new BigDecimal(args[0]);
+        new TaxCalculatorCLI(new TaxCalculator(new InMemoryTaxRepository())).calculate(amount);
+    }
+}
